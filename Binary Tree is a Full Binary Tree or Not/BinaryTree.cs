ba92a80d95923e5a783d568844603be08ae4c1f1
Ripon Datta <ripondatta.us@gmail.com>
2019-06-11 22:47:54 -0700

    Add project files.

---
+++ Binary Tree is a Full Binary Tree or Not/BinaryTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree_is_a_Full_Binary_Tree_or_Not
+{
+    public class BinaryTree
+    {
+        public Node Root;
+
+        public bool IsFullBinaryTree()
+        {
+            return IsFullBinaryTree(Root);
+        }
+
+        private bool IsFullBinaryTree(Node node)
+        {
+            if(node == null)
+            {
+                return true;
+            }
+
+            if(node.Left == null && node.Right == null)
+            {
+                return true;
+            }
+
+            if(node.Left != null && node.Right != null)
+            {
+                return IsFullBinaryTree(node.Left) && IsFullBinaryTree(node.Right);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Binary Tree is a Full Binary Tree or Not/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree_is_a_Full_Binary_Tree_or_Not
+{
+    public class Node
+    {
+        public int Data;
+        public Node Left, Right;
+
+        public Node(int data)
+        {
+            Data = data;
+            Left = Right = null;
+        }
+    }
+}
